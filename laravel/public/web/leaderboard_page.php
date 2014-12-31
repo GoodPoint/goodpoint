5b10583e12e353692d0d88d079bd022a69d996cb
Sreenath K Pillai <pillai.sreenath@gmail.com>
2014-12-31 11:25:09 -0600

    skp

---
+++ laravel/public/web/leaderboard_page.php
@@ -0,0 +1,10 @@
+<div id="leaderboard_page" data-role="page">
+	<div data-role="header">GoodPoint Leaderboard</div>
+	<div class="ui-content">
+		<!--leaderboard-->
+		<ul id="the_leaderboard" data-role="listview">
+			<li></li>
+		</ul>
+	</div>
+	<div data-role="footer">Copyright 2014 GoodPointGame</div>
+</div>
