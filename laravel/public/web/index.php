addcc92a210e17a4a5f7484e20356259f7590de4
Sreenath Pillai <sreenath@flow-enterprises.com>
2015-01-12 06:02:59 -0600

    skp

--- laravel/public/web/index.php
+++ laravel/public/web/index.php
@@ -2,8 +2,10 @@
 <html>
 	<head>
 		<!---->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!---->
 		<link href="css/styles.css" type="text/css" />
-		<link href="jqMobile/jquery.mobile-1.4.5.min.css" type="text/css" />
+		<link rel="stylesheet" href="jqMobile/jquery.mobile-1.4.5.min.css" type="text/css" />
 		<!---->
 		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 		<script src="jqMobile/jquery.mobile-1.4.5.min.js"></script>
