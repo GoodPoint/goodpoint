d7a6cba9492b11bbf1aea137dfb04878755d9b90
Sreenath Pillai <sreenath@flow-enterprises.com>
2015-01-12 08:37:09 -0600

    skp

--- laravel/public/web/player_profile.php
+++ laravel/public/web/player_profile.php
@@ -14,7 +14,7 @@
 		<script>
 			function sendPic() {
 				$('progress').show();
-				var file = myInput.files[0];
+				//var file = myInput.files[0];
 				// Send file here either by adding it to a `FormData` object 
 				// and sending that via XHR, or by simply passing the file into 
 				// the `send` method of an XHR instance.
