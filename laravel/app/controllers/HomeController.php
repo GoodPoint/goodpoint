fffedcf94b15171f8081681fdda522b21b451bbf
Sreenath Pillai <sreenath@flow-enterprises.com>
2015-01-12 10:53:07 -0600

    skp

--- laravel/app/controllers/HomeController.php
+++ laravel/app/controllers/HomeController.php
@@ -89,7 +89,7 @@ class HomeController extends BaseController {
 								$insert = Queries::initUser($_REQUEST['From']);
 								$insert = Queries::initUser($old_owner);
 								//get link to use in message
-								$_link = GAPI::urlShorten("http://54.149.200.91/winwin/laravel/public/web/leaderboard_page.php?sid=".$_REQUEST['MessageSid'])["id"];
+								$_link = GAPI::urlShorten("http://54.149.200.91/winwin/laravel/public/web/leaderboard_page.php?sid=".$_REQUEST['MessageSid']);
 								$message = TwilioMsg::transactionSuccessful($barcode_id, $_link, $_REQUEST['From']);
 								$step = 2;
 								$ab = "a_success";
@@ -154,7 +154,7 @@ class HomeController extends BaseController {
 									$insert = Queries::initUser($_REQUEST['From']);
 									$insert = Queries::initUser($body);
 									//get link to use in message
-									$_link = GAPI::urlShorten("http://54.149.200.91/winwin/laravel/public/web/leaderboard_page.php?sid=".$_REQUEST['MessageSid'])["id"];
+									$_link = GAPI::urlShorten("http://54.149.200.91/winwin/laravel/public/web/leaderboard_page.php?sid=".$_REQUEST['MessageSid']);
 									$message = TwilioMsg::transactionNewPrevOwnerSuccess($barcode_id, $_link, $_REQUEST['From']);
 									$step = 2;
 									$ab = "a_success2";
@@ -248,7 +248,7 @@ class HomeController extends BaseController {
 								$insert = Queries::initUser($_REQUEST['From']);
 								$insert = Queries::initUser($oldOwner);
 								//get link to use in message
-								$_link = GAPI::urlShorten("http://54.149.200.91/winwin/laravel/public/web/leaderboard_page.php?sid=".$_REQUEST['MessageSid'])["id"];
+								$_link = GAPI::urlShorten("http://54.149.200.91/winwin/laravel/public/web/leaderboard_page.php?sid=".$_REQUEST['MessageSid']);
 								$message = TwilioMsg::transactionSuccessful($barcode_id, $_link, $_REQUEST['From']);
 								$step = 2;
 								$ab = "b_success";
