5333284952da16fae12be2697edd7200b31ef883
Sreenath Pillai <sreenath@flow-enterprises.com>
2015-01-16 00:37:39 -0600

    skp

--- laravel/public/web/leaderboard_page.php
+++ laravel/public/web/leaderboard_page.php
@@ -33,9 +33,12 @@
 						//$("#Edit").popup("open");
 					}
 				});
-				$("#leaderboard_page").on("pagecontainerload",function(event,ui){
+				/*$("#leaderboard_page").on("pagecontainerload",function(event,ui){
 					$("#Edit").popup("open");
-				});
+				});*/
+				$(":jqmData(role='page'):last").on("pageshow", function(event) {
+                  $("#Edit", $(this)).popup("open");
+                });
 			});
 		</script>
 		<script src="jqMobile/jquery.mobile-1.4.5.min.js"></script>
