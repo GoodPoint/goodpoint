e3c94fce834875ad560cba57c26a693ffd8172e2
Sreenath Pillai <sreenath@flow-enterprises.com>
2015-05-18 18:46:30 -0500

    skp

--- laravel/app/controllers/includes/gapi.php
+++ laravel/app/controllers/includes/gapi.php
@@ -20,7 +20,8 @@
 			return $r->json()["id"];
 			*/
 			$uri = "http://po.st/api/shorten";
-			$querystring = "?longURL=http%3A%2F%2F54.149.200.91%2Fwinwin%2Flaravel%2Fpublic%2Fweb%2Fleaderboard_page.php%3Fsid%3D".$sid."%26first%3Dtrue&apiKey=D638C739-28D4-48B5-9A1F-ECE285DB5B88";
+			//$querystring = "?longURL=http%3A%2F%2F54.149.200.91%2Fwinwin%2Flaravel%2Fpublic%2Fweb%2Fleaderboard_page.php%3Fsid%3D".$sid."%26first%3Dtrue&apiKey=D638C739-28D4-48B5-9A1F-ECE285DB5B88";
+			$querystring = "?longURL=http%3A%2F%2Fgoodpointgame.com%2Fwinwin%2Flaravel%2Fpublic%2Fweb%2Fleaderboard_page.php%3Fsid%3D".$sid."%26first%3Dtrue&apiKey=D638C739-28D4-48B5-9A1F-ECE285DB5B88";
 			$client = new Client();
 			$r = $client->get($uri.$querystring);
 			return $r->json()["short_url"];
