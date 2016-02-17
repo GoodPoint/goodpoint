d8d3dbd968f291725adb8da24b8e754a3114916a
Sreenath Pillai <sreenath@flow-enterprises.com>
2016-02-17 14:03:27 -0600

    kp

--- laravel/app/controllers/includes/arrays.php
+++ laravel/app/controllers/includes/arrays.php
@@ -4,7 +4,7 @@ class Arrays {
 	public static function vendorArr(){ 
 		return array(0=>"IANS",
 					1=>"TEDDYWEDGERS",
-					2=>"PhitnessPlus",
+					2=>"PHITNESSPLUS",
 					3=>"BEHOPPY");
 	}
 	/*1. 1 slice of cheese or pepperoni pizza - 5GP
@@ -14,12 +14,12 @@ class Arrays {
 		return array(
 		"IANS"=>array(array("name"=>"1 slice of cheese or pepperoni pizza","cost"=>5,"code"=>"IANSSLICE1_"),array("name"=>"Small Salad","cost"=>5,"code"=>"IANSSALAD1_"),array("name"=>"Premium Slice","cost"=>10,"code"=>"IANSSLICE2_")),
 		"TEDDYWEDGERS"=>array(array("name"=>"Half Pasty (of your choice)","cost"=>3,"code"=>"TEDDYHALF"),array("name"=>"Full Pasty","cost"=>5,"code"=>"TEDDYFULL")),
-		"PhitnessPlus"=>array(array("name"=>"Class Schedule","cost"=>"http://bitsy.in/6116b","code"=>""),array("name"=>"Yoga Class","cost"=>6,"code"=>"A"),array("name"=>"C.H.A.M.P Camp","cost"=>6,"code"=>"B"),array("name"=>"F.I.T.T Canp","cost"=>6,"code"=>"C"),array("name"=>"WERQ Class","cost"=>6,"code"=>"D"),array("name"=>"Meditation Clinic","cost"=>6,"code"=>"E")),
+		"PHITNESSPLUS"=>array(array("name"=>"Class Schedule","cost"=>"http://bitsy.in/6116b","code"=>""),array("name"=>"Yoga Class","cost"=>6,"code"=>"A"),array("name"=>"C.H.A.M.P Camp","cost"=>6,"code"=>"B"),array("name"=>"F.I.T.T Canp","cost"=>6,"code"=>"C"),array("name"=>"WERQ Class","cost"=>6,"code"=>"D"),array("name"=>"Meditation Clinic","cost"=>6,"code"=>"E")),
 		"BEHOPPY"=>array(array("name"=>"Postcard","cost"=>1, "code"=>"A")),
 		);
 	}
 	public static function minimumPurchaseArr(){
-		return array("IANS"=>5, "TEDDYWEDGERS"=>3, "PhitnessPlus"=>6, "BEHOPPY"=>1);
+		return array("IANS"=>5, "TEDDYWEDGERS"=>3, "PHITNESSPLUS"=>6, "BEHOPPY"=>1);
 	}
 }
 ?>
