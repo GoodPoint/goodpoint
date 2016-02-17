084cd9d48411ac644ab988fd658bd15db27bfa3e
Sreenath Pillai <sreenath@flow-enterprises.com>
2016-02-17 14:00:02 -0600

    kp

--- laravel/app/controllers/includes/arrays.php
+++ laravel/app/controllers/includes/arrays.php
@@ -15,7 +15,7 @@ class Arrays {
 		"IANS"=>array(array("name"=>"1 slice of cheese or pepperoni pizza","cost"=>5,"code"=>"IANSSLICE1_"),array("name"=>"Small Salad","cost"=>5,"code"=>"IANSSALAD1_"),array("name"=>"Premium Slice","cost"=>10,"code"=>"IANSSLICE2_")),
 		"TEDDYWEDGERS"=>array(array("name"=>"Half Pasty (of your choice)","cost"=>3,"code"=>"TEDDYHALF"),array("name"=>"Full Pasty","cost"=>5,"code"=>"TEDDYFULL")),
 		"PhitnessPlus"=>array(array("name"=>"Class Schedule","cost"=>"http://bitsy.in/6116b","code"=>""),array("name"=>"A) Yoga Class","cost"=>6,"code"=>"A"),array("name"=>"B) C.H.A.M.P Camp","cost"=>6,"code"=>"B"),array("name"=>"C) F.I.T.T Canp","cost"=>6,"code"=>"C"),array("name"=>"D) WERQ Class","cost"=>6,"code"=>"D"),array("name"=>"E) Meditation Clinic","cost"=>6,"code"=>"E")),
-		"BEHOPPY"=>array(array("name"=>"A) Postcard","cost"=>1, "code"=>"A"),
+		"BEHOPPY"=>array(array("name"=>"A) Postcard","cost"=>1, "code"=>"A")),
 		);
 	}
 	public static function minimumPurchaseArr(){
